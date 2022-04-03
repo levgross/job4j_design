3128213b95069ef36d9ae7f64936b87f32db11d9
Grossevich Lev <levgross@gmail.com>
2022-04-03 19:34:37 +0400

    0.1. FileOutputStream. [#252488 #283754]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultFile {
 
@@ -16,7 +17,7 @@ public class ResultFile {
                 }
                 out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
