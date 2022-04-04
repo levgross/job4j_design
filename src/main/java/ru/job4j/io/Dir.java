9168ffe3df7fc79346bb347097cff7830a255334
Grossevich Lev <levgross@gmail.com>
2022-04-04 21:28:01 +0400

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subFile : file.listFiles()) {
+            if (subFile.isFile()) {
+                System.out.println(String.format("name: %s", subFile.getName())
+                                    + String.format(", size: %s", subFile.length()));
+            }
+        }
+    }
+}
