7b4536641bf82cfc345502713c5180d1b86ece05
Grossevich Lev <levgross@gmail.com>
2022-04-05 10:56:30 +0400

    2. Анализ доступности сервера. [#859 #284378]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,27 +1,25 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> inList = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source));
                 PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            in.lines().forEach(inList :: add);
             String[] array;
-            for (int i = 0; i < inList.size(); i++) {
-                array = inList.get(i).split(" ");
+            String prev = "";
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                array = line.split(" ");
                 if ("400".equals(array[0]) || "500".equals(array[0])) {
-                        if (i == 0 || inList.get(i - 1).startsWith("200") || inList.get(i - 1).startsWith("300")) {
+                        if (prev.startsWith("200") || prev.startsWith("300")) {
                             out.print(array[1] + ";");
                         }
                 } else {
-                    if (i != 0 && (inList.get(i - 1).startsWith("400") || inList.get(i - 1).startsWith("500"))) {
+                    if ((prev.startsWith("400") || prev.startsWith("500"))) {
                         out.println(array[1] + ";");
                     }
                 }
+                prev = line;
             }
         } catch (IOException e) {
             e.printStackTrace();
