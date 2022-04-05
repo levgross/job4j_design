bc62fb86d1b4f9b647dd40ddb49b66ad167d448a
Grossevich Lev <levgross@gmail.com>
2022-04-05 21:39:27 +0400

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -0,0 +1,15 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+
+public class DuplicatesFinder {
+    public static void main(String[] args) throws IOException {
+        DuplicatesVisitor dv = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), dv);
+        dv.getDuplicates()
+                .forEach(System.out::println);
+    }
+}
---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,31 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DuplicatesVisitor  extends SimpleFileVisitor<Path> {
+    public Map<FileProperty, String> paths = new HashMap<>();
+    public Set<String> duplicates = new HashSet<>();
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fp = new FileProperty(file.toFile().getTotalSpace(), file.toFile().getName());
+        if (paths.containsKey(fp)) {
+            duplicates.add(paths.get(fp));
+            duplicates.add(file.toFile().getAbsolutePath());
+        }
+        paths.put(fp, file.toFile().getAbsolutePath());
+        return super.visitFile(file, attrs);
+    }
+
+    public Set<String> getDuplicates() {
+
+        return duplicates;
+    }
+}
