fca2bf9bef830355b9c3bdac20920df967327a9d
Grossevich Lev <levgross@gmail.com>
2022-04-06 16:29:39 +0400

    5. Валидация параметров запуска. [#246865 #284988]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,11 +9,21 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null or file extension is null."
-                    + " Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION.");
+                    + " Usage java -jar search.jar ROOT_FOLDER .FILE_EXTENSION.");
         }
         Path start = Paths.get(args[0]);
+        if (!start.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", start.toFile().getAbsoluteFile()));
+        }
+        if (!start.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", start.toFile().getAbsoluteFile()));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException(String.format("Not a file extension %s. "
+                    + "Usage java -jar search.jar ROOT_FOLDER .FILE_EXTENSION", args[1]));
+        }
         List<Path> list = search(start, p -> p.toFile().getName().endsWith(args[1]));
         System.out.println(list.size());
     }
