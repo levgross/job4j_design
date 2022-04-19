370c0d8ffbccba5508f903af08bdbb3df0e8d000
Grossevich Lev <levgross@gmail.com>
2022-04-19 13:20:55 +0400

    1. Inner join [#373617]

--- SQL/join_as.sql
+++ SQL/join_as.sql
@@ -19,6 +19,16 @@ insert into student (name, course, speciality_id) values ('Bob', 1, 2);
 insert into student (name, course, speciality_id) values ('Andrew', 2, 1);
 insert into student (name, course, speciality_id) values ('Jim', 2, 3);
 
-select * from student as st join speciality as sp on st.speciality_id = sp.id;
-select st.name as student, sp.name as spec, sp.professor from student as st join speciality as sp on st.speciality_id = sp.id;
-select sp.professor, sp.name as spec, sp.active, st.course from student as st join speciality as sp on st.speciality_id = sp.id;
+select * from student as st 
+join speciality as sp 
+on st.speciality_id = sp.id;
+
+select st.name as student, sp.name as spec, sp.professor 
+from student as st 
+join speciality as sp 
+on st.speciality_id = sp.id;
+
+select sp.professor, sp.name as spec, sp.active, st.course 
+from student as st 
+join speciality as sp 
+on st.speciality_id = sp.id;
