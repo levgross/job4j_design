47499eba9c04124207efbe40f96439d1f155d0a3
Grossevich Lev <levgross@gmail.com>
2022-04-20 19:26:55 +0400

    0.1. Statement [#379306]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -2,23 +2,50 @@ package ru.job4j.jdbc;
 
 import ru.job4j.io.Config;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.StringJoiner;
 
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        public static Connection getConnection() throws Exception {
         Config config = new Config("app.properties");
         config.load();
         Class.forName(config.value("hibernate.connection.driver_class"));
         String url = config.value("hibernate.connection.url");
         String login = config.value("hibernate.connection.username");
         String password = config.value("hibernate.connection.password");
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getUserName());
-            System.out.println(metaData.getURL());
+        return DriverManager.getConnection(url, login, password);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "create table if not exists demo_table(%s, %s);",
+                        "id serial primary key",
+                        "name varchar(255)"
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "demo_table"));
+            }
         }
+    }
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
     }
 }
---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,125 @@
+package ru.job4j.jdbc;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(properties.getProperty("driver_class"));
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void createTable(String tableName) throws SQLException {
+            String sql = String.format(
+                    "create table if not exists %s(%s, %s);",
+                    tableName,
+                    "id serial primary key",
+                    "name text"
+            );
+            action(sql);
+    }
+
+    public void dropTable(String tableName) throws SQLException {
+            String sql = String.format(
+                    "drop table if exists %s;",
+                    tableName
+            );
+            action(sql);
+    }
+
+    public void addColumn(String tableName, String columnName, String type) throws SQLException {
+            String sql = String.format(
+                    "alter table %s add column %s %s;",
+                    tableName,
+                    columnName,
+                    type
+            );
+            action(sql);
+    }
+
+    public void dropColumn(String tableName, String columnName) throws SQLException {
+            String sql = String.format(
+                    "alter table %s drop column %s;",
+                    tableName,
+                    columnName
+            );
+            action(sql);
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
+            String sql = String.format(
+                    "alter table %s rename column %s to %s;",
+                    tableName,
+                    columnName,
+                    newColumnName
+            );
+            action(sql);
+    }
+
+    private void action(String str) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(str);
+        }
+    }
+
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties prop = new Properties();
+        FileInputStream in = new FileInputStream("my.properties");
+        prop.load(in);
+        TableEditor editor = new TableEditor(prop);
+        editor.createTable("people");
+        System.out.println(getTableScheme(editor.connection, "people"));
+        editor.dropTable("people");
+        editor.createTable("people");
+        editor.addColumn("people", "Age", "varchar(255)");
+        System.out.println(getTableScheme(editor.connection, "people"));
+        editor.dropColumn("people", "Age");
+        System.out.println(getTableScheme(editor.connection, "people"));
+        editor.renameColumn("people", "name", "names");
+        System.out.println(getTableScheme(editor.connection, "people"));
+    }
+}
