7c5ea7087f278c1568ca052a1553349858172149
Grossevich Lev <levgross@gmail.com>
2022-09-09 13:13:25 +0400

    Утверждения с исключениями

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkEmptyArgument() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = new String[0];
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("array is empty");
+    }
+
+    @Test
+    void checkValidateNoEqualsSymbol() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = new String[] {"key:value"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain the symbol")
+                .hasMessageContaining(names[0]);
+    }
+
+    @Test
+    void checkValidateNoKey() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = new String[] {"=value"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key")
+                .hasMessageContaining(names[0]);
+    }
+
+    @Test
+    void checkValidateNoValue() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = new String[] {"key="};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value")
+                .hasMessageContaining(names[0]);
+    }
+}
